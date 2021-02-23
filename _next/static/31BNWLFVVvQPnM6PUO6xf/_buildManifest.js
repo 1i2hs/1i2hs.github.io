be19b7bd00db09146268acb699d6828580778c15
1i2hs <oneitwoh@gmail.com>
2021-02-23 17:22:27 +0000

    Deploying to gh-pages from @ 1i2hs/1i2hs.github.io@004f3c69d95fc872b7d8794c897f15c5833ee9ac 🚀

---
+++ _next/static/31BNWLFVVvQPnM6PUO6xf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-d2db359efd3994518867.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-457052196dd96721bfdd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
